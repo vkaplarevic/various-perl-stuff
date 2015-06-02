11acccc694fd0299756bde943d3e712b2b97e154
vlad.kaplarevic@gmail.com <vlad.kaplarevic@gmail.com>
2015-06-02 20:00:16 +0200

    Fast CGI with Perl

---
+++ fcgi-hello.pl
@@ -0,0 +1,30 @@
+#!/usr/bin/env plackup 
+
+use warnings;
+use strict;
+use v5.10;
+use Plack::Handler::FCGI;
+
+my $app = sub 
+{ 
+    my $env = shift;
+    my $env_message = "Environment:\n";
+    $env_message .= (defined($env->{$_}) ? ("$_ => " . $env->{$_} . "\n") : "NONE\n") foreach(keys %{$env});
+
+    my $rsp =  [
+        '200',
+        ['Content-Type' => 'text/plain'],
+        [$env_message],
+    ];
+    return $rsp;
+};
+
+# Create a simple FCGI Server configuration.
+my $server = Plack::Handler::FCGI->new(
+    nproc  => 1,
+    listen => ["localhost:9900"],
+    detach => 0,
+);
+
+$server->run($app);
+
